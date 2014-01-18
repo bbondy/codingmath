49d6fde0778673ac7db1c8b022e63b00eaf51423
Brian R. Bondy <netzen@gmail.com>
2014-01-17 21:33:04 -0500

    Added elliptical 3d looking motion from video 4

--- sin/elliptical_motion/main.js
+++ sin/elliptical_motion/main.js
@@ -4,16 +4,18 @@ function Circle(startingAngle)
 {
   this.angle = startingAngle;
   this.speed = 0.05;
-  this.offset = height * .4;
-  this.speed = 0.05;
+  this.radiusX = height * .4;
+  this.radiusY = height * .2;
+  this.speed = 0.03;
 }
 
 Circle.prototype = {
   render: function() {
-    var y = centerY + Math.sin(this.angle) * this.offset;
-    var x = centerX + Math.cos(this.angle) * this.offset;
+    var y = centerY + Math.sin(this.angle) * this.radiusX;
+    var x = centerX + Math.cos(this.angle) * this.radiusY;
+    var r = 20 + Math.cos(this.angle) * 15;
     context.beginPath();
-    context.arc(x, y, 50, 0, Math.PI * 2, false);
+    context.arc(x, y, r, 0, Math.PI * 2, false);
     context.fill();
 
     this.angle += this.speed;
@@ -30,7 +32,7 @@ window.onload = function() {
   centerX = width * .5;
 
   var circles = [];;
-  var NUM_CIRCLES = 10;
+  var NUM_CIRCLES = 50;
   for (var i = 0; i < NUM_CIRCLES; i++)  {
     circles.push(new Circle(Math.PI * 2 / NUM_CIRCLES * i));
   }
