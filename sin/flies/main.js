ea60ec9dbe608857d9b9df6d3a107d486caadabb
Brian R. Bondy <netzen@gmail.com>
2014-01-17 21:44:01 -0500

    Simmulate flies flying around using Lissajous Curves

---
+++ sin/flies/main.js
@@ -0,0 +1,50 @@
+var centerX, centerY, width, height, context;
+
+function Circle(startingAngle)
+{
+  this.angleX = startingAngle;
+  this.angleY = startingAngle;
+  this.speedX = Math.random() * 0.03;
+  this.speedY = Math.random() * 0.03;
+  this.radiusX = height * .5;
+  this.radiusY = height * .5;
+}
+
+Circle.prototype = {
+  render: function() {
+    var y = centerY + Math.sin(this.angleY) * this.radiusX;
+    var x = centerX + Math.cos(this.angleX) * this.radiusY;
+    var r = 2;
+    context.beginPath();
+    context.arc(x, y, r, 0, Math.PI * 2, false);
+    context.fill();
+
+    this.angleX += this.speedX;
+    this.angleY += this.speedY;
+  }
+};
+
+window.onload = function() {
+  var canvas = document.getElementById("canvas");
+  context = canvas.getContext("2d");
+
+  width = canvas.width = window.innerWidth;
+  height = canvas.height = window.innerHeight;
+  centerY = height * .5;
+  centerX = width * .5;
+
+  var circles = [];;
+  var NUM_CIRCLES = 100;
+  for (var i = 0; i < NUM_CIRCLES; i++)  {
+    circles.push(new Circle(Math.PI * 2 / NUM_CIRCLES * i));
+  }
+
+  function render() {
+    context.clearRect(0, 0, width, height);
+    circles.forEach(function(c) {
+      c.render();
+    });
+    requestAnimationFrame(render);
+  }
+  render();
+};
